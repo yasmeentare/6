57669b4d9451608663e7d74663ac0126307e9064
yasmeentariq <127335577+yasmeentare@users.noreply.github.com>
2023-03-09 09:17:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿var colorsArr = new string[] { "red-" };
+List<string> colors = new List<string>();
+Console.WriteLine(colors.Count);
+colors.Add("RED");
+colors.Add("BLACN");
+colors[5]= "ORANE";
+
+Colors.RemoveAt(3);
+Console.WriteLine(colors.Count);
+for (int i=0; i<colors.Count; i++)
+{
+    Console.WriteLine(colors[1]);
+}
